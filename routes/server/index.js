da03914a322c0a108a8933ac17bbf9a0c4e91309
vulp <tuomo.pohjola@gmail.com>
2016-05-18 22:17:34 +0300

    routes

---
+++ routes/server/index.js
@@ -0,0 +1,24 @@
+var express = require('express');
+var server = express.Router();
+
+server.get('/', function(req, res,next) {		
+	console.log('get');
+	var db = req.db;
+	var collection = db.get('servers');
+	collection.find({},{},function(e,data){		
+		var servers = {
+			'servers':data
+			}
+		res.json(servers);			
+	
+	});
+});
+
+server.post('/add', function(req, res,next) {		
+	console.log('add');
+	var db = req.db;
+	var collection = db.get('servers');
+	collection.insert(req.body);
+});
+
+module.exports.server = server;
