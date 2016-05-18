33b0be4ec6d05cf020bc3925d4dfb345b94e3d31
vulp <tuomo.pohjola@gmail.com>
2016-05-18 22:13:19 +0300

    changed project to use new database and added add function

--- server.js
+++ server.js
@@ -7,23 +7,20 @@ var mongo = require('mongodb');
 var monk = require('monk');
 var db = monk('localhost:27017/nodetest');
 
-var routes = require('./routes/index');
-
 var app = express();
 
 app.set('port', process.env.PORT || 3000);
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
-
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use(function(req,res,next){
+app.use(function(req,res,next){		
     req.db = db;
     next();
 });
 
-app.use('/',routes);
+app.use('/server', require('./routes/server/index').server);
 
 app.listen(app.get('port'), function() {
   console.log('Express server listening on port ' + app.get('port'));
